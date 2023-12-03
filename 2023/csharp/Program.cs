c215d1104148b2fa3bca09b72c1ca87f09d10907
Philipp Reimer <philipp.reimer93@gmail.com>
2023-12-03 13:58:05 -0300

    2023 01 **

---
+++ 2023/csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿string output;
+
+if (args.Length == 0) {
+    output = "Please select a day '01' - '24'";
+} else {
+    output = args[0] switch {
+        "01" => 
+            r3im.aof.d01.Task1.run() + "\n" +
+            r3im.aof.d01.Task2.run(),
+        _ => $"{args[0]} not implemented",
+    };
+}
+
+Console.WriteLine(output);
+
