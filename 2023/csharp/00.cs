2770713825ada6005aa8f45d6d4f95bddf342c69
Philipp Reimer <philipp.reimer93@gmail.com>
2023-12-03 23:01:08 -0300

    template

---
+++ 2023/csharp/00.cs
@@ -0,0 +1,21 @@
+namespace r3im.aof.d00;
+
+public static class Task1 {
+
+    public static string run() {
+        var lines = input.Split('\n');
+        var output = lines
+            .JoinToString('\n')
+            ;
+            
+        return output;
+    }
+
+    static string input = 
+        "";
+        // File.ReadAllText("00.txt").Trim();
+
+// """
+// """;
+
+}
