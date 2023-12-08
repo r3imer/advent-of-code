934980e24a7a593eec94ba3abb47ef81f36dbe3f
Philipp Reimer <philipp.reimer93@gmail.com>
2023-12-08 08:21:11 -0300

    utils update

--- 2023/csharp/Util.cs
+++ 2023/csharp/Util.cs
@@ -3,12 +3,15 @@ using System.Text.Json;
 namespace r3im;
 
 public static class String {
+    static readonly JsonSerializerOptions s_opts = new() {
+        IncludeFields = true,  
+    };
     public static string JoinToString(this IEnumerable<string> arr, char c) 
         => string.Join(c, arr);
     public static string JoinToString(this IEnumerable<string> arr, string s) 
         => string.Join(s, arr);
     public static T LogJson<T>(this T x, string before = "", string after = "") {
-        Console.WriteLine(before + JsonSerializer.Serialize(x) + after);
+        Console.WriteLine(before + JsonSerializer.Serialize(x, s_opts) + after);
         return x;
     }
     public static T Log<T>(this T x, string before = "", string after = "") {
