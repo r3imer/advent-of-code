9b605b070a16fc04aca729932482162b885200d4
Philipp Reimer <philipp.reimer93@gmail.com>
2023-12-09 11:35:51 -0300

    update csharp template

--- 2023/csharp/00.cs
+++ 2023/csharp/00.cs
@@ -1,22 +1,21 @@
 namespace r3im.aof.d00;
 
-public static class Task1 {
+public static class Task {
 
-    public static string run() {
-        var lines = input.Split('\n');
-        var output = lines
-            .Select(x => x)
-            .JoinToString('\n')
-            ;
-            
-        return output;
+    public static string run(string p, string f) {
+        var input = File.ReadAllText(f);
+        return p switch {
+            "1" => part1(input),
+            "2" => part2(input),
+            _ => "",
+        };
     }
 
-    static string input = 
-        "";
-        // File.ReadAllText("00.txt").Trim();
+    public static string part1(string input) {
+        return "";
+    }
 
-// """
-// """;
-
+    public static string part2(string input) {
+        return "";
+    }
 }
